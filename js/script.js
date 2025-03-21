2ad83fa9efd431270096eda8b9c2205fff13b709
GiordanoCasale <giordanocasale8@gmail.com>
2025-03-21 14:56:44 +0100

    [UPD] First exercise completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 //esercizio 1
 //creiamo un prompt per l'inserimento della parola dell'utente
+let word= prompt("inserisci una parola palindroma")
+//andiamo a creare una funzione per fare il check della parola
+function palindroma(text){
+//definiamo una variabile per i vari attributi della parola
+    let checktest= text.split(""). reverse(). join("");
+//creiamo un if per la condizione di ceck
+    if(checktest === text){
+        console.log("Parola palindroma");
+    }
+    else{
+        console.log("Parola non palindroma");
+    }
+}
+
+palindroma(word);
+//Fine esercizio
