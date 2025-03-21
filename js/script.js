636f7e8d5f3b68a08d2dc0dcef3a5ccbc35a144e
GiordanoCasale <giordanocasale8@gmail.com>
2025-03-21 15:21:24 +0100

    [UPD] Function created

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
 //esercizio 1
 //creiamo un prompt per l'inserimento della parola dell'utente
-let word= prompt("inserisci una parola palindroma")
+//let word= prompt("inserisci una parola palindroma")
 //andiamo a creare una funzione per fare il check della parola
-function palindroma(text){
+//function palindroma(text){
 //definiamo una variabile per i vari attributi della parola
-    let checktest= text.split(""). reverse(). join("");
+   // let checktest= text.split(""). reverse(). join("");
 //creiamo un if per la condizione di ceck
-    if(checktest === text){
-        console.log("Parola palindroma");
-    }
-    else{
-        console.log("Parola non palindroma");
-    }
-}
+   // if(checktest === text){
+   //     console.log("Parola palindroma");
+   // }
+    //else{
+        //console.log("Parola non palindroma");
+    //}
+//}
 
 palindroma(word);
 //Fine esercizio
 
 //esercizio 2
+//creaiamo un prompt per la scelta del numero
+let numbers = parseInt(prompt("Inserisci un numero da 1 a 5"))
+//creiamo una funzione per la generazione automatica di un numero
+function randomnumber(){
+    return Math.floor((Math.random() * 5) + 1);
+}
 
+function sum(){
+    return 
+}
